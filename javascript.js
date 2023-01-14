92a8f285ccb0b131223a3269efbef05269f61b60
ALFIO BATTIATO <112937226+AlfioBattiato@users.noreply.github.com>
2023-01-14 21:01:10 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+let button=document.getElementById('exit');
+    login=document.getElementById('login');
+
+button.onclick=function ciao(){
+    console.log('ciao');
+    login.style.display='none';
+
+
+
+    
+}
